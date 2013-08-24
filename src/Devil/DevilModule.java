543dcee409716d49cccac9857b687fd356303695
Shvedov Yury <shved@lvk.cs.msu.su>
2013-08-24 21:33:39 +0000

    Making src dir

--- src/Devil/DevilModule.java
+++ src/Devil/DevilModule.java
@@ -4,4 +4,11 @@ public abstract class DevilModule {
     private String moduleName;
     public abstract void runModule (Devil devil);
     public abstract void stopModule ();
+
+    void setModuleName (String name) {
+        moduleName = name
+    }
+    String getModuleName () {
+        return moduleName;
+    }
 }
