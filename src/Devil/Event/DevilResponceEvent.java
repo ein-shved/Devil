c6df347f86d672a1e4a24d3c78320b929aea1edc
Shvedov Yury <shved@lvk.cs.msu.su>
2013-08-26 19:04:59 +0000

    Licence hints added.

--- src/Devil/Event/DevilResponceEvent.java
+++ src/Devil/Event/DevilResponceEvent.java
@@ -1,3 +1,22 @@
+/* Devil is the complex messager with distributed structure.
+ * Copyright (C) 2013  Shvedov Yury
+ * 
+ * This file is part of Devil.
+ *
+ * Devil is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Devil is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Devil.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package Devil.Event;
 
 public abstract class DevilResponceEvent extends DevilEvent {
